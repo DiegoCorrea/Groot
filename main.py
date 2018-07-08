607ad0f326047771b70b9e38a623cec094410acb
Diego Correa <diegocorrea.cc@gmail.com>
2018-07-08 13:08:23 -0300

    chamando arvore de decisão

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data.setHandle import load_data_users, load_data_songs, extractSet
 from statisticalOverview import statisticalOverview
-
+from decisionTree import make_set_to_process, preprocessing_data, tree_execute
 
 def restartSet():
     extractSet()
@@ -10,8 +10,10 @@ def restartSet():
 def main():
     SONGSET = load_data_songs()
     PREFERENCESET = load_data_users()
-    statisticalOverview(SONGSET, PREFERENCESET)
-
+    dict_set = statisticalOverview(SONGSET, PREFERENCESET)
+    data_set = make_set_to_process(SONGSET, dict_set)
+    clean_data_set = preprocessing_data(data_set)
+    tree_execute(clean_data_set)
 
 if __name__ == "__main__":
     main()
