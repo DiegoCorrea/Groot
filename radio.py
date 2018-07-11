947b4142c5b2f0125d9b78dac3c807b2de7191ac
Diego Correa <diegocorrea.cc@gmail.com>
2018-07-11 12:16:36 -0300

    adicionando matrix de distancia

--- radio.py
+++ radio.py
@@ -2,21 +2,28 @@ class Radio:
     def __init__(self, song_set, preference_set):
         self.song_set = song_set
         self.preference_set = preference_set
-        self.weight = None
+        self.classifier = None
         self.features = None
         self.targets = None
+        self.distance_matrix = None
 
     def get_preference_set(self):
         return self.preference_set
 
-    def get_song_set(self):
-        return self.song_set
-
     def post_preference_set(self, preference_set):
         self.preference_set = preference_set
 
-    def post_weight(self, weight):
-        self.weight = weight
+    def get_song_set(self):
+        return self.song_set
 
-    def get_weight(self):
-        return self.weight
+    def post_classifier(self, new_classifier):
+        self.classifier = new_classifier
+
+    def get_classifier(self):
+        return self.classifier
+
+    def set_distance_matrix(self, new_distance_matrix):
+        self.distance_matrix = new_distance_matrix
+
+    def get_distance_matrix(self):
+        return self.distance_matrix
